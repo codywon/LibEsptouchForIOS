d4f1327535ef317ae34f73c458d6e89f5c3b3172
afunx <afun1989@gmail.com>
2016-06-20 15:54:20 +0800

    Esptouch v0.3.4.3 only support Espressif's Smart Config v2.4

---
+++ LibEsptouchForIOSDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LibEsptouchForIOSDemo
+//
+//  Created by 白 桦 on 6/20/16.
+//  Copyright © 2016 白 桦. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ESPAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ESPAppDelegate class]));
+    }
+}
